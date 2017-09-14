b031059c1883ecfd96b8fef3535837b8da4b5248
liufeng <1048769816@qq.com>
2017-09-14 17:41:45 +0800

    test

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <loading :com='compelete'></loading>
+  </div>
+</template>
+
+<script>
+import Loading from './Loading.vue'
+export default {
+  name: 'hello',
+  components:{ Loading },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      compelete:10
+    }
+  },
+  methods:{
+    counTime:function(){
+      var that = this;
+      var time = setInterval(function(){
+        that.compelete--;
+      },100);
+    }
+  },
+  mounted (){
+    // this.counTime();
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="loading">
+		<div class="load_animate">
+			<span class="data-custom-animate" v-for="loadlist in loadAnimate"></span>
+			<p>Loading...</p>
+			<p v-text='com'></p>
+			
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name:'loading',
+		data(){
+			return {
+				loadAnimate:[0,1,2,3,4],
+			}
+		},
+		props:['com'],
+		methods:{
+			stepLoading:function(){
+				var time = setInterval(function(){
+
+				},200);
+			}
+		},
+		mounted(){
+			console.log(this.com);
+		},
+		watch:{
+			com:function(n,o){
+				console.log(n,'------',o)
+			}
+		}
+	}
+</script>
+<style scoped>
+	.load_animate span{
+		display: inline-block;
+		width: 12px;
+		height: 12px;
+		border-radius: 50%;
+		background: #ababab;
+		margin: 0 5px;
+	}
+	.load_animate span:first-child{
+		width: 16px;
+		height: 16px;
+	}
+	.load_animate .data-custom-animtae-active{
+		width: 16px;
+		height: 16px;
+	}
+</style>
